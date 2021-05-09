14ab7fe4e82c393048df7c787b477632f9670ffc
Gayal Seneviratne <gayals40@gmail.com>
2021-05-09 09:08:57 +0530

    SinglyLinkList Class Initialized

--- src/SinglyLinkListTest.java
+++ src/SinglyLinkListTest.java
@@ -1,5 +1,8 @@
+import ds.SinglyLinkList;
+
 public class SinglyLinkListTest {
     public static void main(String[] args) {
-
+        SinglyLinkList linkList = new SinglyLinkList();
+//        linkList.add(10);
     }
 }
--- src/ds/SinglyLinkList.java
+++ src/ds/SinglyLinkList.java
@@ -1,4 +1,39 @@
 package ds;
 
 public class SinglyLinkList {
+    private Node first;
+    private Node last;
+
+
+    public void add(int number){
+//        System.out.println(number);
+    }
+
+    public void add(int index, int number){
+
+    }
+
+    public void remove(int index){
+
+    }
+
+    public int get(int index){
+        return -1;
+    }
+
+    public void print(){
+
+    }
+
+    public int size(){
+        return -1;
+    }
+
+    public boolean contains(int number){
+        return false;
+    }
+
+    public boolean empty(){
+        return false;
+    }
 }
