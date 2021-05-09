515fefafa03d478d76589b8df00bb98db8f30afa
Gayal Seneviratne <gayals40@gmail.com>
2021-05-09 09:01:20 +0530

    Node class encapsulated

--- src/ds/Node.java
+++ src/ds/Node.java
@@ -1,5 +1,35 @@
 package ds;
 
 public class Node {
+    private int value;
+    private Node next;
 
+    public Node() {
+    }
+
+    public Node(int value) {
+        this.setValue(value);
+
+    }
+
+    public Node(int value, Node next) {
+        this.setValue(value);
+        this.setNext(next);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
