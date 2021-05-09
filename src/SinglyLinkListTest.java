9a80811112af815e7522497124fd90a66b40a226
Gayal Seneviratne <gayals40@gmail.com>
2021-05-09 20:09:52 +0530

    Small issues fixed and done another test run

--- src/SinglyLinkListTest.java
+++ src/SinglyLinkListTest.java
@@ -1,4 +1,3 @@
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import ds.SinglyLinkList;
 
 public class SinglyLinkListTest {
@@ -28,15 +27,38 @@ public class SinglyLinkListTest {
 //        linkList.add(0,11111);
 //        linkList.print();
 //        linkList.add(4,11111);
-//        linkList.print();
+//        linkList.print();import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 //        linkList.add(1,11111);
 //        linkList.print();
 //        linkList.remove(0);
 //        linkList.print();
 //        System.out.println(linkList.size());
-        System.out.println(linkList.get(0));
-        System.out.println(linkList.get(4));
+//        System.out.println(linkList.get(0));
+//        System.out.println(linkList.get(4));
 //        System.out.println(linkList.get(-1));
 //        System.out.println(linkList.get(5));
+//        linkList.add(0,1000);
+//        linkList.print();
+//        linkList.add(4,1000);
+//        linkList.print();
+//        linkList.add(5,1000);
+//        linkList.add(-1,1000);
+//        linkList.remove(0);
+//        linkList.remove(4);
+//        linkList.remove(3);
+//        linkList.remove(-1);
+//        linkList.remove(5);
+//        linkList.print();
+        linkList.print();
+        System.out.println(linkList.get(0));
+        System.out.println(linkList.get(4));
+        System.out.println(linkList.get(3));
+//        System.out.println(linkList.get(-1));
+//        System.out.println(linkList.get(5));
+        System.out.println(linkList.size());
+        System.out.println(linkList.empty());
+        linkList.clear();
+        linkList.print();
+
     }
 }
--- src/ds/SinglyLinkList.java
+++ src/ds/SinglyLinkList.java
@@ -51,29 +51,33 @@ public class SinglyLinkList {
         } else {
             Node current = first;
             if (index == 0) {
-                first = current.getNext();
-            } else if (index == size() - 1) {
+                Node next = current.getNext();
+                current.setNext(null);
+                first = next;
+            }
+
+            if (index == size() - 1) {
                 while (current != null) {
                     if (current.getNext() == last) {
                         current.setNext(null);
                         last = current;
                         break;
-
                     }
                     current = current.getNext();
                 }
 
-            } else {
-                int count = 0;
-                while (current != null) {
-                    if (count == index - 1) {
-                        Node temp = current.getNext();
-                        current.setNext(temp.getNext());
-                        temp.setNext(null);
-                    }
-                    current = current.getNext();
-                    count++;
+            }
+
+            int count = 0;
+            while (current != null) {
+                if (count == index - 1) {
+                    Node node = current.getNext();
+                    Node next = node.getNext();
+                    current.setNext(next);
+                    node.setNext(null);
                 }
+                current = current.getNext();
+                count++;
             }
         }
         size--;
@@ -81,18 +85,27 @@ public class SinglyLinkList {
     }
 
     public int get(int index) {
-        if(empty()){
+        if (empty()) {
             throw new RuntimeException("Cannot get item from empty Link List");
         }
-        if(index > size()-1 || index < 0){
+        if (index > size() - 1 || index < 0) {
             throw new RuntimeException("Index Error");
         }
+
+        if(index == 0){
+            return first.getValue();
+        }
+
+        if(index == size()-1){
+            return last.getValue();
+        }
+
         Node current = first;
         int count = 0;
         int value = 0;
 
-        while(current != null){
-            if(count == index){
+        while (current != null) {
+            if (count == index) {
                 value = current.getValue();
             }
             current = current.getNext();
